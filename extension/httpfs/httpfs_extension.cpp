c9b478ca0e921e1353a470b72b3d6c23e88c2a5e
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-06-18 14:39:39 +0200

    Skip openssl / AES logic

--- extension/httpfs/httpfs_extension.cpp
+++ extension/httpfs/httpfs_extension.cpp
@@ -70,7 +70,7 @@ static void LoadInternal(DatabaseInstance &instance) {
 	CreateBearerTokenFunctions::Register(instance);
 
 	// set pointer to OpenSSL encryption state
-	config.encryption_util = make_shared_ptr<AESStateSSLFactory>();
+	//config.encryption_util = make_shared_ptr<AESStateSSLFactory>();
 }
 void HttpfsExtension::Load(DuckDB &db) {
 	LoadInternal(*db.instance);
